62b6cd19f6d866e1ad6a6dbbe87cc8f7e4483459
LINAULIA <51009783+LINAULIA@users.noreply.github.com>
2019-05-26 17:20:51 +0700

    TA DKP

---
+++ Kasir_Salon/src/kasir_salon/Kata.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kasir_salon;
+
+/**
+ *
+ * @author Lina Aulia
+ */
+public class Kata {
+    public String Awal(){
+  return ("Biaya yang harus dibayar : ");
+  }
+    
+}
